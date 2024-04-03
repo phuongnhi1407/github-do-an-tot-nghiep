023055da2c432c7250cb5589e9a99f5ef859bf6f
phuongnhi1407 <lhpnhi.92.TCV@gmail.comm>
2024-04-03 10:22:22 +0700

    git push

---
+++ lib/src/modules/authen/pages/home.dart
@@ -0,0 +1,46 @@
+import 'package:doantotnghiep/src/modules/authen/pages/Login.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: [
+        TextButton(
+            onPressed: () async {
+              final SharedPreferences? prefs = await _prefs;
+              prefs?.clear();
+              Get.offAll(AuthScreens());
+            },
+            child: Text(
+              'logout',
+              style: TextStyle(color: Colors.white),
+            ))
+      ]),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Welcome home'),
+            TextButton(
+                onPressed: () async {
+                  final SharedPreferences? prefs = await _prefs;
+                  print(prefs?.get('token'));
+                },
+                child: Text('print token'))
+          ],
+        ),
+      ),
+    );
+  }
+}
