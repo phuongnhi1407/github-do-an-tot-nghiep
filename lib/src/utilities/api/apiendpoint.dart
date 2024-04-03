c45b7fda6db55feafd81f96faabc57e46ace83d5
phuongnhi1407 <lhpnhi.92.TCV@gmail.comm>
2024-04-03 10:22:35 +0700

    git push

---
+++ lib/src/utilities/api/apiendpoint.dart
@@ -0,0 +1,9 @@
+class ApiEndPoints {
+  static final String baseUrl = 'http://base_url/api/';
+  static _AuthEndPoints authEndpoints = _AuthEndPoints();
+}
+
+class _AuthEndPoints {
+  final String registerEmail = 'authaccount/registration';
+  final String loginEmail = 'authaccount/login';
+}
