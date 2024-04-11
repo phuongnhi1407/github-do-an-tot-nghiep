246f7bc1486ccf318a9842ca1982956acfdf0c21
admin <admin@gmail.com>
2024-04-11 17:44:29 +0700

    git push

--- lib/src/modules/authen/dtos/request/login_request.dart
+++ lib/src/modules/authen/dtos/request/login_request.dart
@@ -5,13 +5,13 @@ class LoginRequest {
   LoginRequest({this.username, this.password});
 
   LoginRequest.fromJson(Map<String, dynamic> json) {
-    username = json['username'];
+    username = json['userName'];
     password = json['password'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['username'] = this.username;
+    data['userName'] = this.username;
     data['password'] = this.password;
     return data;
   }
